4b79e79456007c4dc49e2f8aa6d1418aeee3bb24
savagePy <fernando.dataops@outlook.com>
2022-02-16 14:58:11 -0300

    Initial Commit

---
+++ modulo1/ex002.py
@@ -0,0 +1,3 @@
+nome = input('Qual e o seu nome? ')
+print('É um grande prazer te conhecer, {}!'.format(nome))
+
---
+++ scripts/desafio003.py
@@ -0,0 +1,5 @@
+"""Script que leia dos números e mostre a soma entre eles."""
+
+n1 = int(input('Primeiro número:'))
+n2 = int(input('Segundo número:'))
+print('A soma é',n1+n2)
